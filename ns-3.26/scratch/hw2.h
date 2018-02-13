f47bf96e9f6fcae3e38e382f01672ded28635ea2
kwo227 <kwo227@tamu.edu>
2018-02-13 03:10:15 -0600

    homework finally done Jesus

---
+++ ns-3.26/scratch/hw2.h
@@ -0,0 +1,18 @@
+#ifndef HW2_H
+#define HW2_H
+
+#include <ns3/propagation-loss-model.h>
+#include <ns3/propagation-environment.h>
+#include "ns3/log.h"
+#include "ns3/double.h"
+#include "ns3/enum.h"
+#include "ns3/mobility-model.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "string.h"
+#include <cmath>
+
+#endif
